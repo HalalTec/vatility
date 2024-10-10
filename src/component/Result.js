6cc6b6f4f1447252ec6722a23b7383b2c9ab78e3
HalalTec <abdulkadirshuaib4real@gmail.com>
2024-10-10 15:38:22 +0100

    finale update

--- src/component/Result.js
+++ src/component/Result.js
@@ -7,12 +7,12 @@ import * as am5xy from "@amcharts/amcharts5/xy";
 import * as am5radar from '@amcharts/amcharts5/radar';
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 const Result = ({
-    career, money, per, rel, fun, physical, spirit, health 
+    career, money, per, rel, fun, physical, spirit, health, purpose, contribution 
 
         }) => {
 
           
-           
+           console.log(per[0], per[1])
 
             useEffect(() => {
                 // Create root element
@@ -94,14 +94,17 @@ const Result = ({
         
                 // Set data
                 const data = [
-                    { category: 'Health', currentValue: health[0], desiredValue: health[1], columnSettings: { fill: chart.get('colors').next() } },
+                    { category: 'Phy. & Emot. H', currentValue: health[0], desiredValue: health[1], columnSettings: { fill: chart.get('colors').next() } },
                     { category: 'Career', currentValue: career[0], desiredValue: career[1], columnSettings: { fill: chart.get('colors').next() } },
-                    { category: 'Physical Environment', currentValue: physical[0], desiredValue: physical[1], columnSettings: { fill: chart.get('colors').next() } },
+                    { category: 'Living & Work Env.', currentValue: physical[0], desiredValue: physical[1], columnSettings: { fill: chart.get('colors').next() } },
                     { category: 'Spirituality', currentValue: spirit[0], desiredValue: spirit[1], columnSettings: { fill: chart.get('colors').next() } },
-                    { category: 'Peronal Growth', currentValue: per[0], desiredValue: per[1], columnSettings: { fill: chart.get('colors').next() } },
-                    { category: 'Money', currentValue: money[0], desiredValue: money[1], columnSettings: { fill: chart.get('colors').next() } },
-                    { category: 'Fun and Recreation', currentValue: fun[0], desiredValue: fun[1], columnSettings: { fill: chart.get('colors').next() } },
-                    { category: 'Relationships', currentValue: rel[0], desiredValue: rel[1], columnSettings: { fill: chart.get('colors').next() } },
+                    { category: 'Per. Growth', currentValue: per[0], desiredValue: per[1], columnSettings: { fill: chart.get('colors').next() } },
+                    { category: 'Fin. Health', currentValue: money[0], desiredValue: money[1], columnSettings: { fill: chart.get('colors').next() } },
+                    { category: 'Fun & Recr.', currentValue: fun[0], desiredValue: fun[1], columnSettings: { fill: chart.get('colors').next() } },
+                    { category: 'Rel. & S. Life', currentValue: rel[0], desiredValue: rel[1], columnSettings: { fill: chart.get('colors').next() } },
+                    { category: 'Purpose', currentValue: purpose[0], desiredValue: purpose[1], columnSettings: { fill: chart.get('colors').next() } },
+                    { category: 'Contr & Legacy', currentValue: contribution[0], desiredValue: contribution[1], columnSettings: { fill: chart.get('colors').next() } },
+
                 ];
         
                 currentSeries.data.setAll(data);
@@ -122,14 +125,18 @@ const Result = ({
 
 
             const categories = [
-                { name: 'career', values: [career[0], career[1], career[0] - career[1]] },
-                { name: 'money', values: [money[0], money[1], money[0] - money[1]] },
+                { name: 'Career and Professional Growth', values: [career[0], career[1], career[0] - career[1]] },
+                { name: 'Financial Health', values: [money[0], money[1], money[0] - money[1]] },
                 { name: 'personal growth', values: [per[0], per[1], per[0] - per[1]] },
-                { name: 'relationships', values: [rel[0], rel[1], rel[0] - rel[1]] },
+                { name: 'Relationships and Social Life', values: [rel[0], rel[1], rel[0] - rel[1]] },
                 { name: 'fun and recreation', values: [fun[0], fun[1], fun[0] - fun[1]] },
-                { name: 'physical environment', values: [physical[0], physical[1], physical[0] - physical[1]] },
-                { name: 'spirituality', values: [spirit[0], spirit[1], spirit[0] - spirit[1]] },
-                { name: 'health', values: [health[0], health[1], health[0] - health[1]] }
+                { name: 'Living & Work Environment', values: [physical[0], physical[1], physical[0] - physical[1]] },
+                { name: 'Spirituality and Inner Growth', values: [spirit[0], spirit[1], spirit[0] - spirit[1]] },
+                { name: 'Physical & Emotional Health ', values: [health[0], health[1], health[0] - health[1]] },
+                { name: 'Purpose & Fulfillment ', values: [purpose[0], purpose[1], purpose[0] - purpose[1]] },
+                { name: 'Contribution & Legacy ', values: [contribution[0], contribution[1], contribution[0] - contribution[1]] }
+
+
             ];
 
             const sortedCategories = [...categories].sort((a, b) => b.values[2] - a.values[2]);
@@ -168,7 +175,7 @@ const Result = ({
             <div className="result" >
                 <div className="pie">
                     {/* <img src={res} alt="" srcSet="" /> */} 
-                    <div id="chartdiv" style={{ width: "500px", height: "500px" }}></div>
+                    <div id="chartdiv" className='chartdiv'></div>
 
                 </div>
 
--- src/component/Wheel.js
+++ src/component/Wheel.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import {  useState } from "react";
 import Career from "./Career";
 import Result from "./Result";
 
 const Wheel = () => {
     const [counter, setCounter] = useState(1);
     const [style, setSty] = useState({backgroundColor: "#ED6A56"})
-    const [p, setP] = useState("Create")
+    const [p, setP] = useState("Career and Professional Growth")
     const [career, setCareer] = useState([])
     const [health, setHealth] = useState([])
     const [money, setMoney] = useState([])
@@ -14,120 +14,186 @@ const Wheel = () => {
     const [fun, setFun] = useState([])
     const [physical, setPhysical] = useState([])
     const [spirit, setSpirit] = useState([])
+    const [cont, setCont] = useState([])
+    const [pur, setPur] = useState([])
 
+   
     const submit = (e) => {
         e.preventDefault();
         let ans= e.target.textContent
         setCounter(counter + 1);
-        console.log(career)
 
         if (counter <= 2) {
-            setCareer((prev) =>  [...prev, ans])
-                
-
+            setCareer([...career, ans]);
+            // console.log(counter)
+            // console.log(career)
         }
-            if (counter >= 2) {
-                setSty({backgroundColor: "#F09B36"})
-                setP("Money")
-                setMoney((prev) =>  [...prev, ans])
+
+       
+        if (counter ===2 && counter < 4) {
+            setSty({ backgroundColor: "#F09B36" });
+            setP("Financial Health");
+          
+        } 
+       if(counter > 2 && counter <= 4){ 
+            setMoney([...money, ans]);
+            console.log(counter)
+            console.log(money)
+        }
+        
+        
+         if (counter ===4 && counter < 6) {
+            setSty({ backgroundColor: "#FFC74E" });
+            setP("Physical & Emotional Health");
             
-                    
+        } 
+        
+        if(counter > 4 && counter <= 6){ 
+            setHealth([...health, ans]);
+            console.log(health);
+            console.log(counter);
+        }
+        
+        if (counter === 6 && counter < 8) {
+            setSty({ backgroundColor: "#A0B470" });
+            setP("Relationships and Social Life");
+        }
+        
+        if(counter > 6 && counter <= 8){ 
+            setRel([...rel, ans]);
+            console.log(rel);
+            console.log(counter);
+        }
+        
+        if (counter === 8 && counter < 10) {
+            setSty({ backgroundColor: "#6E9E75" });
+            setP("Personal Growth");
+        } 
+        if(counter > 8 && counter <= 10){ 
+            setPer((e) => [...e, ans]);
+            console.log(counter);
+        }
+        if (counter === 10 && counter < 12) {
+            setSty({ backgroundColor: "#73CCE4" });
+            setP("Fun and Recreation");
+        } 
+        if(counter > 10 && counter <= 12){ 
+            setFun((p) => [...p, ans]);
+            console.log(counter);
+        }
+        
+         if (counter === 12 && counter < 14) {
+            setSty({ backgroundColor: "#869ACF" });
+            setP("Living & Working Environment");
+        } 
 
-            }
-            if (counter >= 4) {
-                setSty({backgroundColor: "#FFC74E"})
-                setP("Health")
-                setHealth((prev) =>  [...prev, ans])
+        if(counter > 12 && counter <= 14){ 
+            setPhysical((res) => [...res, ans]);
+            console.log(counter);
+        }
 
-            }
-            if (counter >= 6) {
-                setSty({backgroundColor: "#A0B470"})
-                setP("Relationships")
-                setRel((prev) =>  [...prev, ans])
-            }
-            if (counter >= 8) {
-                setSty({backgroundColor: "#6E9E75"})
-                setP("Personal Growth")
-                setPer((prev) =>  [...prev, ans])
-            }
-            if (counter >= 10) {
-                setSty({backgroundColor: "#73CCE4"})
-                setP("Fun and Recreation")
-                setFun((prev) =>  [...prev, ans])
-            }
-            if (counter >= 12) {
-                setSty({backgroundColor: "#869ACF"})
-                setP("Physical Environment")
-                setPhysical((prev) =>  [...prev, ans])
-            }
-            if (counter >= 14) {
-                setSty({backgroundColor: "895881"})
-                setP("Spirituality")
-                setSpirit((prev) =>  [...prev, ans])
+        if (counter === 14 && counter < 16) {
+            setSty({ backgroundColor: "#895881" });
+        } 
 
-            }
+        if(counter > 14 && counter <= 16){ 
+            setSpirit((re) => [...re, ans]);
+            console.log(counter);
+        }
 
-            
+         if (counter === 16 && counter < 18) {
+            setSty({ backgroundColor: "#895681" });
+            setP("Purpose and Fulfillment");
+        } 
+
+        if(counter > 16 && counter <= 18){ 
+            setPur((v) => [...v, ans]);
+            console.log(counter);
+        }
+
+         if (counter === 18 && counter < 20) {
+            setSty({ backgroundColor: "#895981" });
+            setP("Contribution and Legacy");
+        }
+        
+        if(counter > 18 && counter <= 20){ 
+            setCont((pr) => [...pr, ans]);
+            console.log(counter);
+        }
+
+
 
     }
 
     return ( 
        
        <div>
-                     {counter <= 15 && (   <header style={style}> {p}  </header> )}
+                     {counter <= 20 && (   <header style={style}> {p}  </header> )}
                 <div className="section">
                 {counter === 1 && (
                 <Career question="How do you feel about your career right now?"/>
             )}
                 {counter === 2 && (
-                <Career question=" How would you like to feel about your career in the future?" style={{textAlign:"right"}} />
+                <Career question=" How would you like to feel about your career in the future?" />
             )}
 
         {counter === 3 && (
                 <Career question=" How do you feel about your money right now?" />
             )}
         {counter === 4 && (
-                <Career question="How would you like to feel about your money in the future?" style={{textAlign:"right"}}/>
+                <Career question="How would you like to feel about your money in the future?" />
             )}
         {counter === 5 && (
                 <Career question="How do you feel about your health right now?" />
             )}
         {counter === 6 && (
-                <Career question="How would you like to feel about your health in the future?" style={{textAlign:"right"}}/>
+                <Career question="How would you like to feel about your health in the future?" />
             )}
         {counter === 7 && (
                 <Career question="How do you feel about your relationships right now?" />
             )}
         {counter === 8 && (
-                <Career question="How would you like to feel about your relationships in the future?" style={{textAlign:"right"}}/>
+                <Career question="How would you like to feel about your relationships in the future?" />
             )}
         {counter === 9 && (
                 <Career question="How do you feel about your personal growth right now?" />
             )}
         {counter === 10 && (
-                <Career question="How would you like to feel about your personal growth in the future?" style={{textAlign:"right"}}/>
+                <Career question="How would you like to feel about your personal growth in the future?" />
             )}
          {counter === 11 && (
                 <Career question="How do you feel about your fun and recreation right now?" />
             )}
         {counter === 12 && (
-                <Career question="How would you like to feel about your fun and recreation in the future?" style={{textAlign:"right"}}/>
+                <Career question="How would you like to feel about your fun and recreation in the future?" />
             )}
         {counter === 13 && (
                 <Career question="How do you feel about your physical environment right now?" />
             )}
         {counter === 14 && (
-                <Career question="How would you like to feel about your physical environment in the future?" style={{textAlign:"right"}} />
+                <Career question="How would you like to feel about your physical environment in the future?" />
             )}
         {counter === 15 && (
                 <Career question="How do you feel about your spirituality right now?" />
             )}
         {counter === 16 && (
-                <Career question="How would you like to feel about your spirituality in the future?" style={{textAlign:"right"}}/>
+                <Career question="How would you like to feel about your spirituality in the future?" />
+            )}
+        {counter === 17 && (
+                <Career question="How do you feel about your purpose and fullfillment right now?" />
+            )}
+         {counter === 18 && (
+                <Career question="How would you like to feel about your purpose and fullfillment in the future?" />
+            )}
+         {counter === 19 && (
+                <Career question="How do you feel about your contribution and legacy right now?" />
+            )}
+         {counter === 20 && (
+                <Career question="How would you like to feel about your contribution and legacy in the future?" />
             )}
            
 
-            {counter <= 16 && ( 
+            {counter <= 20 && ( 
                     <ol>
                 <span onClick={submit}> 1 </span>
                 <span onClick={submit}>2</span>
@@ -145,7 +211,7 @@ const Wheel = () => {
                 {counter <= 16 && ( 
                     <footer style={style} className="foot"> </footer>
                 )}
-                    {counter > 16 && (
+                    {counter > 20 && (
                 <Result career={career}  
                         money={money}
                         per= {per}
@@ -154,7 +220,9 @@ const Wheel = () => {
                     physical = {physical} 
                     spirit = {spirit}
                     health = {health}
-
+                    contribution = {cont}
+                    purpose = {pur}
+                
 
                 />
             )}
